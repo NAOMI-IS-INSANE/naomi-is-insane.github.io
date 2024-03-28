c0684bdc70cf545ca4da3d289aa397801d443df1
Naomi Been It <165294777+NAOMI-IS-INSANE@users.noreply.github.com>
2024-03-29 02:19:17 +1030

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+passwd
+apt update
+apt install ufw apache2 python3-certbot-apache
+ls /etc/apache2/sites-available/
+a2dissite 000-default
+rm /etc/apache2/sites-available/000-default.conf
+rm /etc/apache2/sites-available/default-ssl.conf
+mkdir /var/www/naomis-world.com/
+mkdir /var/www/naomis-world.com/public_html
+curl -o /var/www/naomis-world.com/public_html/index.html https://naomi-is-insane.github.io/server/index.html
+curl -o /etc/apache2/sites-available/naomis-world.conf https://naomi-is-insane.github.io/server/naomis-world.conf
+a2ensite naomis-world
+certbot --apache
+systemctl reload apache2
