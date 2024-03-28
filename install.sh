2ce0d08829723d2d3a4aeb95bbb4c60d4547b694
Naomi Been It <165294777+NAOMI-IS-INSANE@users.noreply.github.com>
2024-03-29 03:29:36 +1030

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ export LANG=en_AU.UTF-8
 
 # Update locale to English (Australia)
 echo "en_AU.UTF-8 UTF-8" > /etc/locale.gen
-locale-gen
+locale-gen > /var/myLogs/1-locale 2>&1
 update-locale LANG=en_AU.UTF-8
 
 # Set timezone to Adelaide
@@ -35,15 +35,15 @@ passwd
 check_status "passwd"
 
 # Update package list
-apt update > /var/myLogs/1-apt-update 2>&1
+apt update > /var/myLogs/2-apt-update 2>&1
 check_status "apt update"
 
 # Install necessary packages
-apt install -y ufw apache2 python3-certbot-apache > /var/myLogs/2-apt-install 2>&1
+apt install -y ufw apache2 python3-certbot-apache > /var/myLogs/3-apt-install 2>&1
 check_status "apt install"
 
 # Enable firewall
-echo "yes" | ufw enable > /var/myLogs/3-ufw 2>&1
+echo "yes" | ufw enable > /var/myLogs/4-ufw 2>&1
 check_status "ufw enable"
 
 # Allow SSH, HTTP, and HTTPS
@@ -52,7 +52,7 @@ ufw allow 80
 ufw allow 443
 
 # Remove default Apache config files
-a2dissite 000-default > /var/myLogs/4-2dissite 2>&1
+a2dissite 000-default > /var/myLogs/5-2dissite 2>&1
 rm /etc/apache2/sites-available/000-default.conf
 rm /etc/apache2/sites-available/default-ssl.conf
 
@@ -70,7 +70,7 @@ if [ $? -ne 0 ]; then
     echo "Unable to download naomis-world.conf"
     exit 1
 fi
-a2ensite naomis-world > /var/myLogs/5-a2ensite 2>&1
+a2ensite naomis-world > /var/myLogs/6-a2ensite 2>&1
 
 # Obtain SSL certificate
 certbot --apache
