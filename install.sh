00f6158bcc467f1f9baf21e18324b0067e2ba35d
Naomi Been It <165294777+NAOMI-IS-INSANE@users.noreply.github.com>
2024-03-29 03:50:14 +1030

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ mkdir /var/myLogs/
 
 # Set locale variables
 export LANGUAGE=en_AU.UTF-8
+export LC_ALL=en_AU.UTF-8
+export LC_TIME=en_AU.UTF-8
 export LANG=en_AU.UTF-8
 
 # Update locale to English (Australia)
